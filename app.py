b83b18499a78acc4a7f816c100773fd769838f94
yvonnegichovi <yvonnegichovi@gmail.com>
2025-02-03 10:52:48 +0300

    enabled CORS

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify
-from datetime import datetime
+from flask_cors import CORS
+from datetime import datetime, timezone
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def index():
+    current_utc = datetime.utcnow().replace(microsecond=0)
     return jsonify({
         "email": "yvonnegichovi@gmail.com",
-        "current_datetime": datetime.utcnow().isoformat() + "Z",
+        "current_datetime": current_utc.isoformat() + "Z",
         "github_url": "https://github.com/yvonnegichovi/HNG-Internship-c12"
     })
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
